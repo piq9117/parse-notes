b7a5aa7a091386eb447b3f2eedb4d84243c6edab
piq9117 <piq9117@gmail.com>
2024-10-17 01:09:52 -0400

    wip: stream notes to db

--- src/Notes/File.hs
+++ src/Notes/File.hs
@@ -2,16 +2,18 @@
 
 module Notes.File (parseFile) where
 
+import Conduit ((.|))
 import Conduit qualified
+import Crypto.Hash.MD5 (hash)
 import Notes.Parser (parseNotes)
 
 parseFile :: FilePath -> IO ()
 parseFile filepath =
   Conduit.runConduitRes $
     Conduit.sourceFile filepath
-      Conduit..| Conduit.mapMC
-        ( \content -> do
-            -- TODO insert to sqllite
-            print $ parseNotes (decodeUtf8 content)
+      .| Conduit.mapC (parseNotes <<< decodeUtf8)
+      .| Conduit.concatMapC identity
+      .| Conduit.mapM_C
+        ( \notes ->
+            print $ hash (show @ByteString notes)
         )
-      Conduit..| Conduit.sinkNull
